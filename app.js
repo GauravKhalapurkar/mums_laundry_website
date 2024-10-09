e1d35250cf2c65a39fe05e9d89e0d9d2ddc7888f
Gaurav K <gaurav.khalapurkar@gmail.com>
2024-10-10 02:32:12 +0530

    app.js updated

--- app.js
+++ app.js
@@ -2,10 +2,13 @@ const express = require('express');
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
 const app = express();
+const path = require('path');
 
 app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static("public"));
+// app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get("/", function(req, res) {
     res.render("index");
